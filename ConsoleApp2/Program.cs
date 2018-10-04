4d54a912c379162e44df27714de543b8f28ba2f9
phoenixzero4 <phoenixzero4@hotmail.com>
2018-10-04 10:15:32 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class tests
+    {
+        public string concat(string x, string y)
+        {
+            string result = x + y;
+            return result;
+        }
+        public string cpy(string x)
+        {
+            string copy = x;
+            return copy;
+        }
+        public bool testEmpty(string x)
+        {
+            if (x == "")
+                return true;
+            return false;
+        }
+        public bool cmpr(string x, string y)
+        {
+            if (x == y) return true;
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test()
+        {
+            string a = "Hello";
+            string b = "World!";
+            string x = "HelloWorld!";
+            ConsoleApp2.tests t = new ConsoleApp2.tests();
+            a = t.concat(a, b);
+           
+            Assert.AreEqual(x, a);
+            string copy = t.cpy(a);
+            Assert.AreEqual(copy, a);
+            string empty = "";
+            Boolean ff = t.testEmpty(empty);
+            Assert.IsFalse(ff);
+           // Assert.IsFalse(t.testEmpty(empty)); // should fail
+            Assert.IsTrue(t.cmpr(a, a));
+          //  Assert.IsFalse(t.cmpr(a, a));  // should fail
+          
+          
+        }
+    }
+}
